468abaed2197f547ed879687cc95c943d6834c32
Rahul <rahulbhataniya2468@gmail.com>
2021-07-23 10:29:23 +0530

    done

--- flask_backend.py
+++ flask_backend.py
@@ -6,7 +6,7 @@ app = Flask(__name__, static_folder='static')
 @app.route("/")
 def success():
     path='que_ans_video.mp4'
-    path='videoplayback.mp4'
+    #path='videoplayback.mp4'
     #time_subtitle=[]
     time_subtitle = swt.get_large_audio_transcription('static'+'/'+path)
     time_subtitle.append(path)
